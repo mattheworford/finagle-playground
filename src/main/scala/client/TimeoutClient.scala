000eb82149161107d3ff5229a2f34819fc80144b
Matthew Orford <mattorford9@gmail.com>
2024-03-23 17:14:00 -0400

    Timeout filter and client (#5)

---
+++ src/main/scala/client/TimeoutClient.scala
@@ -0,0 +1,21 @@
+package client
+
+import com.twitter.finagle.{Http, Service}
+import com.twitter.finagle.http.{Method, Request, Response}
+import com.twitter.finagle.service.TimeoutFilter
+import com.twitter.util.{Duration, Future, JavaTimer}
+
+object TimeoutClient {
+  def main(args: Array[String]): Unit = {
+    val originalClient: Service[Request, Response] = Http.newService("localhost:9090")
+    val timeoutFilter = new TimeoutFilter[Request, Response](Duration.fromSeconds(1), new JavaTimer())
+    val filteredClient = timeoutFilter.andThen(originalClient)
+
+    val request = Request(Method.Get, "/?name=matthew")
+    val response: Future[Response] = filteredClient(request)
+
+    response.onSuccess(resp => println(resp.getContentString()))
+    response.onFailure(ex => ex.printStackTrace())
+    Thread.sleep(2000)
+  }
+}
---
+++ src/main/scala/filter/TimeoutFilter.scala
@@ -0,0 +1,9 @@
+package filter
+
+import com.twitter.finagle.{Service, SimpleFilter}
+import com.twitter.util.{Duration, Future, Timer}
+
+class TimeoutFilter[Req, Rep](timeout: Duration, timer: Timer) extends SimpleFilter[Req, Rep] {
+  override def apply(request: Req, service: Service[Req, Rep]): Future[Rep] =
+    service(request).within(timer, timeout)
+}
