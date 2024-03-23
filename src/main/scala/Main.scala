3b0fdc2f39628e7bb5efd408baa4f81e5c7ada20
Matthew Orford <mattorford9@gmail.com>
2024-03-23 15:58:46 -0400

    Simple server/client implementation (#4)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,15 +2,10 @@
 import com.twitter.finagle.{Http, Service}
 import com.twitter.finagle.http
 import com.twitter.util.{Await, Future}
+import service.StringLengthService
 
 object Main extends App {
-  val service = new Service[http.Request, http.Response] {
-    def apply(req: http.Request): Future[http.Response] =
-      Future.value(
-        http.Response(req.version, http.Status.Ok)
-      )
-  }
-
+  val service = StringLengthService.init()
   val server = Http.serve(":9090", service)
   Await.ready(server)
 }
