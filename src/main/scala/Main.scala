028ae0e883959565d8db227d7a1af1e14e4a40cd
Matthew Orford <mattorford9@gmail.com>
2024-03-23 15:18:54 -0400

    initialize finagle server (#3)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,16 @@
+
+import com.twitter.finagle.{Http, Service}
+import com.twitter.finagle.http
+import com.twitter.util.{Await, Future}
+
 object Main extends App {
-  println("Hello, World!")
+  val service = new Service[http.Request, http.Response] {
+    def apply(req: http.Request): Future[http.Response] =
+      Future.value(
+        http.Response(req.version, http.Status.Ok)
+      )
+  }
+
+  val server = Http.serve(":9090", service)
+  Await.ready(server)
 }
