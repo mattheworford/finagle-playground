3f02ab298d63e206f8864e9b34ae879838be88f0
Matthew Orford <mattorford9@gmail.com>
2024-03-24 10:24:21 -0400

    Add scalafmt for formatting (#8)

--- src/main/scala/service/StringLengthService.scala
+++ src/main/scala/service/StringLengthService.scala
@@ -5,17 +5,23 @@ import com.twitter.finagle.http.{Request, Response, Status}
 import com.twitter.util.Future
 
 object StringLengthService {
-  def init(): Service[Request, Response] = {
-    (req: Request) => {
-      val nameOption = Option(req.getParam("string"))
-      nameOption match {
-        case Some(name) => Future.value(createResponse(req, name.length.toString, Status.Ok))
-        case None => Future.value(createResponse(req, "Missing 'string' parameter", Status.BadRequest))
-      }
+  def init(): Service[Request, Response] = { (req: Request) =>
+    val nameOption = Option(req.getParam("string"))
+    nameOption match {
+      case Some(name) =>
+        Future.value(createResponse(req, name.length.toString, Status.Ok))
+      case None =>
+        Future.value(
+          createResponse(req, "Missing 'string' parameter", Status.BadRequest)
+        )
     }
   }
 
-  private def createResponse(req: Request, content: String, status: Status): Response = {
+  private def createResponse(
+    req: Request,
+    content: String,
+    status: Status,
+  ): Response = {
     val response = Response(req.version, status)
     response.setContentString(content)
     response
