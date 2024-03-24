0624517922d080cae2450570af1cc37203171714
Matthew Orford <mattorford9@gmail.com>
2024-03-24 17:50:33 -0400

    add client

---
+++ src/main/scala/client/LoadBalancedClient.scala
@@ -0,0 +1,20 @@
+package client
+
+import com.twitter.finagle.http.{Method, Request}
+import com.twitter.finagle.{Address, Http, Name}
+import com.twitter.util.Future
+
+object LoadBalancedClient {
+  def main(args: Array[String]): Unit = {
+    val addresses =
+      (9089 to 9091).toList.map(port => Address("localhost", port))
+    val name: Name = Name.bound(addresses: _*)
+    val client     = Http.newService(name, "client")
+    val requests =
+      (1 to 20).map(i => Request(Method.Get, s"?string=${"matthew" * i}"))
+    val responses = requests.map(req => client(req).map(_.getContentString))
+
+    Future.collect(responses).foreach(println)
+    Thread.sleep(5000)
+  }
+}
