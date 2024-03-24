61021b2fdc8ae401cfcf3fb687fb9c2eb2efe7f8
Matthew Orford <mattorford9@gmail.com>
2024-03-23 23:28:19 -0400

    clean up sls

--- src/main/scala/service/StringLengthService.scala
+++ src/main/scala/service/StringLengthService.scala
@@ -1,18 +1,23 @@
 package service
 
-import com.twitter.finagle.{Http, Service}
-import com.twitter.finagle.http
-import com.twitter.util.{Await, Future}
+import com.twitter.finagle.Service
+import com.twitter.finagle.http.{Request, Response, Status}
+import com.twitter.util.Future
 
 object StringLengthService {
   def init(): Service[Request, Response] = {
-    new Service[Request, Response] {
-      def apply(req: Request): Future[Response] = {
-        val name = req.getParam("name")
-        val response = Response(req.version, Status.Ok)
-        response.setContentString(name.length.toString)
-        Future.value(response)
+    (req: Request) => {
+      val nameOption = Option(req.getParam("string"))
+      nameOption match {
+        case Some(name) => Future.value(createResponse(req, name.length.toString, Status.Ok))
+        case None => Future.value(createResponse(req, "Missing 'string' parameter", Status.BadRequest))
       }
     }
+  }
+
+  private def createResponse(req: Request, content: String, status: Status): Response = {
+    val response = Response(req.version, status)
+    response.setContentString(content)
+    response
   }
 }
